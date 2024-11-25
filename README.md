5ec605def4a139ba028efb14ba7e309bd0fa13f7
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-11-25 10:43:16 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Escape-Game
+================
+
+A simple web game where you have to escape a room.
---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+require_once("config.php");
+
+    $db = new PDO('mysql:host='.$config['db_host'].';dbname='.$config['dbname'], $config['db_user'], $config['db_pass']);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $sth = $db->execute("SELECT * FROM users WHERE username = $_POST[username] AND password = $_POST[password]");
+        $user = $sth->fetch(PDO::FETCH_ASSOC);
+        if ($user && password_verify($_POST['password'], $user['password'])) {
+            echo "Connexion réussie! Bienvenue, " . htmlspecialchars($user['id']) . ".";
+        } else {
+            echo "Nom d'utilisateur ou mot de passe incorrect.";
+        }
+    }
+
+?>
+
+<!-- HTML Form -->
+<form action="login.php" method="post">
+    <label for="username">Nom d'utilisateur :</label>
+    <input type="text" id="username" name="username" required><br><br>
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password" required><br><br>
+    <input type="submit" value="Connexion">
+</form>
