0fb2c3224d215984a4ab124802b3fc29a08991e7
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-11-26 15:08:08 +0100

    fixed

--- page2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$valid_code = 'B7R8J2D';
-
-if (isset($_GET['code']) && $_GET['code'] === $valid_code) {
-    echo "Bravo ! Vous avez accédé au défi suivant.";
-} else {
-    echo "Code invalide.";
-}
-?>
